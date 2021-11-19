d46db6fe19a7ed2347a337e051b3b115d1932d64
MilKMS <84441925+MilKMS@users.noreply.github.com>
2021-11-19 17:19:46 +0900

    Update settings.py

--- noweb/noweb/noweb/settings.py
+++ noweb/noweb/noweb/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$w4ye5f2xf^@(d!nxgx%3e2_ll+#-ichz&@3qc5kdce0=1pwye'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
